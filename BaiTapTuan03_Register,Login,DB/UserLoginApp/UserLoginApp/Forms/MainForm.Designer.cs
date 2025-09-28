eb4d23f1b4300c40d32ed41d7ba67737c22bf71e
HoangPhu <hoangphu140906@gmail.com>
2025-09-28 20:29:34 +0700

    design mainform

--- BaiTapTuan03_Register,Login,DB/UserLoginApp/UserLoginApp/Forms/MainForm.Designer.cs
+++ BaiTapTuan03_Register,Login,DB/UserLoginApp/UserLoginApp/Forms/MainForm.Designer.cs
@@ -28,46 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.grUserInfo = new System.Windows.Forms.GroupBox();
+            this.lbEmail = new System.Windows.Forms.Label();
+            this.lbUsername = new System.Windows.Forms.Label();
             this.lbWelcome = new System.Windows.Forms.Label();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnLogOut = new System.Windows.Forms.Button();
+            this.grUserInfo.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // grUserInfo
+            // 
+            this.grUserInfo.Controls.Add(this.lbEmail);
+            this.grUserInfo.Controls.Add(this.lbUsername);
+            this.grUserInfo.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grUserInfo.Location = new System.Drawing.Point(20, 90);
+            this.grUserInfo.Name = "grUserInfo";
+            this.grUserInfo.Size = new System.Drawing.Size(580, 180);
+            this.grUserInfo.TabIndex = 4;
+            this.grUserInfo.TabStop = false;
+            this.grUserInfo.Text = "Thông tin đăng nhập";
+            // 
+            // lbEmail
+            // 
+            this.lbEmail.Location = new System.Drawing.Point(30, 80);
+            this.lbEmail.Name = "lbEmail";
+            this.lbEmail.Size = new System.Drawing.Size(500, 30);
+            this.lbEmail.TabIndex = 1;
+            this.lbEmail.Text = "Email";
+            // 
+            // lbUsername
+            // 
+            this.lbUsername.Location = new System.Drawing.Point(30, 40);
+            this.lbUsername.Name = "lbUsername";
+            this.lbUsername.Size = new System.Drawing.Size(500, 30);
+            this.lbUsername.TabIndex = 0;
+            this.lbUsername.Text = "Tên đăng nhập";
             // 
             // lbWelcome
             // 
-            this.lbWelcome.AutoSize = true;
-            this.lbWelcome.Location = new System.Drawing.Point(213, 61);
+            this.lbWelcome.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbWelcome.Location = new System.Drawing.Point(20, 20);
             this.lbWelcome.Name = "lbWelcome";
-            this.lbWelcome.Size = new System.Drawing.Size(58, 16);
-            this.lbWelcome.TabIndex = 0;
-            this.lbWelcome.Text = "Xin chào";
-            this.lbWelcome.Click += new System.EventHandler(this.label1_Click);
+            this.lbWelcome.Size = new System.Drawing.Size(400, 40);
+            this.lbWelcome.TabIndex = 3;
+            this.lbWelcome.Text = "Xin chào, ";
             // 
-            // groupBox1
+            // btnLogOut
             // 
-            this.groupBox1.Location = new System.Drawing.Point(425, 152);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(200, 100);
-            this.groupBox1.TabIndex = 1;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.btnLogOut.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnLogOut.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogOut.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogOut.Location = new System.Drawing.Point(330, 330);
+            this.btnLogOut.Name = "btnLogOut";
+            this.btnLogOut.Size = new System.Drawing.Size(140, 40);
+            this.btnLogOut.TabIndex = 5;
+            this.btnLogOut.Text = "Đăng xuất";
+            this.btnLogOut.UseVisualStyleBackColor = false;
+            this.btnLogOut.Click += new System.EventHandler(this.btnLogOut_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.grUserInfo);
             this.Controls.Add(this.lbWelcome);
+            this.Controls.Add(this.btnLogOut);
             this.Name = "MainForm";
-            this.Text = "MainForm";
+            this.Text = "Trang chủ";
+            this.grUserInfo.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.GroupBox grUserInfo;
+        private System.Windows.Forms.Label lbEmail;
+        private System.Windows.Forms.Label lbUsername;
         private System.Windows.Forms.Label lbWelcome;
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnLogOut;
     }
 }
