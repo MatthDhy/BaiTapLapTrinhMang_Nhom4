6e284bdc7461e694bcb9cfa003eb7e4dbbc34166
DHuyyy <24520656@gm.uit.edu.vn>
2025-10-02 00:16:27 +0700

    done database

--- BaiTapTuan03_Register,Login,DB/UserLoginApp/UserLoginApp/Helpers/DatabaseHelper.cs
+++ BaiTapTuan03_Register,Login,DB/UserLoginApp/UserLoginApp/Helpers/DatabaseHelper.cs
@@ -135,5 +135,6 @@ namespace UserLoginApp.Helpers
                 return null;
             }
         }
+
     }
 }
--- BaiTapTuan03_Register,Login,DB/UserLoginApp/UserLoginApp/SQL/setup.sql
+++ BaiTapTuan03_Register,Login,DB/UserLoginApp/UserLoginApp/SQL/setup.sql
@@ -1,29 +1,37 @@
+﻿-- Tạo database nếu chưa có
 IF DB_ID('CaroAppDB') IS NULL
 BEGIN
-	CREATE DATABASE CaroAppDB;
+    CREATE DATABASE CaroAppDB;
 END
 GO
 
 USE CaroAppDB;
 GO
 
-IF OBJECT_ID('dbo.Users', 'U') IS NOT NULL
-    DROP TABLE dbo.Users;
+-- Tạo bảng Users nếu chưa có
+IF OBJECT_ID('dbo.Users', 'U') IS NULL
+BEGIN
+    CREATE TABLE Users
+    (
+        UserID INT IDENTITY(1,1) PRIMARY KEY,
+        Username NVARCHAR(50) NOT NULL UNIQUE,
+        Password NVARCHAR(255) NOT NULL,
+        Email NVARCHAR(100) NULL,
+        CreatedAt DATETIME DEFAULT GETDATE()
+    );
+END
 GO
 
-CREATE TABLE USERS
-(
-	UserID INT IDENTITY(1,1) PRIMARY KEY,   
-    Username NVARCHAR(50) NOT NULL UNIQUE,  
-    Password NVARCHAR(255) NOT NULL,    
-    Email NVARCHAR(100) NULL,               
-    CreatedAt DATETIME DEFAULT GETDATE()
-);
+-- Chèn user mặc định nếu chưa có
+IF NOT EXISTS (SELECT * FROM Users WHERE Username = N'Huy')
+BEGIN
+    INSERT INTO Users (Username, Password, Email)
+    VALUES (N'Huy', 
+            N'8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 
+            N'1234@gmail.com');
+END
 GO
 
-INSERT INTO USERS (Username, Password, Email)
-VALUES (N'Huy', N'8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', N'1234@gmail.com');
-GO
-
-SELECT * FROM USERS;
+-- Test
+SELECT * FROM Users;
 GO
