67ec8b378d1515dbd28a812cba4dcbe539ca7c83
TRAN LE VAN HIEU <tranhieu.0971311425@gmail.com>
2025-10-27 15:48:47 +0700

    add UI and FormServer

--- BaiTapTuan04_MultipleSocket/UserManageApp/Server/Forms/FormServer.Designer.cs
+++ BaiTapTuan04_MultipleSocket/UserManageApp/Server/Forms/FormServer.Designer.cs
@@ -28,13 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(68, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server Status";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(120, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Running on port 9000";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.Color.Red;
+            this.btnStart.Location = new System.Drawing.Point(72, 116);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(127, 61);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start Server";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.ForeColor = System.Drawing.Color.Red;
+            this.btnStop.Location = new System.Drawing.Point(350, 116);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(127, 61);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop Server";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(68, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Log:";
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(25, 231);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(722, 277);
+            this.txtLog.TabIndex = 5;
+            this.txtLog.TextChanged += new System.EventHandler(this.txtLog_TextChanged);
+            // 
+            // FormServer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(759, 730);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormServer";
+            this.Text = "FormServer";
+            this.Load += new System.EventHandler(this.FormServer_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtLog;
     }
 }
 
--- BaiTapTuan04_MultipleSocket/UserManageApp/Server/Forms/FormServer.cs
+++ BaiTapTuan04_MultipleSocket/UserManageApp/Server/Forms/FormServer.cs
@@ -7,14 +7,67 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Server.Models;
 
 namespace Server
 {
     public partial class FormServer : Form
     {
+        private ServerCore server;
         public FormServer()
         {
             InitializeComponent();
+        }
+
+        private void txtLog_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormServer_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private  void btnStart_Click(object sender, EventArgs e)
+        {
+            if (server == null)
+            {
+                int port = 9000;
+                string connStr = "Data Source=.;Initial Catalog=UserDB;Integrated Security=True"; // hoặc chuỗi kết nối thật
+
+                server = new ServerCore(port, connStr);
+
+                server.OnLog += LogMessage;
+                server.OnClientConnected += ip => LogMessage($"Client connected: {ip}");
+                server.OnClientDisconnected += ip => LogMessage($"Client disconnected: {ip}");
+
+                server.Start();
+
+                LogMessage($"✅ Server started on port {port}");
+                btnStart.Enabled = false;
+                btnStop.Enabled = true;
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            server?.Stop();
+            LogMessage("🛑 Server stopped.");
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+        }
+
+        private void LogMessage(string msg)
+        {
+            if (txtLog.InvokeRequired)
+            {
+                txtLog.Invoke(new Action<string>(LogMessage), msg);
+            }
+            else
+            {
+                txtLog.AppendText($"[{DateTime.Now:HH:mm:ss}] {msg}\r\n");
+            }
         }
     }
 }
