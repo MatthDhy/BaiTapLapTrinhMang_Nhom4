5ba610c8f7ba036439576defb240916c24b9c8f3
dhuy-uit <cpdhuy@gmail.com>
2025-10-29 23:07:56 +0700

    LoginForm

--- BaiTapTuan04_MultipleSocket/UserManageApp/Client/Forms/LoginForm.Designer.cs
+++ BaiTapTuan04_MultipleSocket/UserManageApp/Client/Forms/LoginForm.Designer.cs
@@ -1,39 +1,239 @@
-﻿namespace Client.Forms
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Client.Forms
 {
     partial class LoginForm
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        private Panel panelShell;
+        private Panel panelLeft;
+        private Panel panelRight;
+        private Label lblTagline;
+
+        private Label lblTitle;
+        private TextBox txtUsername;
+        private TextBox txtPassword;
+        private Button btnTogglePwd;
+        private CheckBox chkRemember;
+        private Button btnLogin;
+        private Button btnRegister;
+        private Label lblError;
+
         protected override void Dispose(bool disposing)
         {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
+            if (disposing && components != null) components.Dispose();
             base.Dispose(disposing);
         }
 
         #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "LoginForm";
-        }
+            this.panelShell = new System.Windows.Forms.Panel();
+            this.panelLeft = new System.Windows.Forms.Panel();
+            this.lblTagline = new System.Windows.Forms.Label();
+            this.panelRight = new System.Windows.Forms.Panel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnTogglePwd = new System.Windows.Forms.Button();
+            this.chkRemember = new System.Windows.Forms.CheckBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.lblError = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelShell.SuspendLayout();
+            this.panelLeft.SuspendLayout();
+            this.panelRight.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelShell
+            // 
+            this.panelShell.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(28)))), ((int)(((byte)(40)))));
+            this.panelShell.Controls.Add(this.panelLeft);
+            this.panelShell.Controls.Add(this.panelRight);
+            this.panelShell.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelShell.Location = new System.Drawing.Point(24, 24);
+            this.panelShell.Name = "panelShell";
+            this.panelShell.Padding = new System.Windows.Forms.Padding(28);
+            this.panelShell.Size = new System.Drawing.Size(1032, 632);
+            this.panelShell.TabIndex = 0;
+            // 
+            // panelLeft
+            // 
+            this.panelLeft.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(24)))), ((int)(((byte)(36)))));
+            this.panelLeft.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelLeft.Controls.Add(this.pictureBox1);
+            this.panelLeft.Controls.Add(this.lblTagline);
+            this.panelLeft.Location = new System.Drawing.Point(48, 48);
+            this.panelLeft.Name = "panelLeft";
+            this.panelLeft.Size = new System.Drawing.Size(360, 560);
+            this.panelLeft.TabIndex = 0;
+            // 
+            // lblTagline
+            // 
+            this.lblTagline.Font = new System.Drawing.Font("Segoe UI", 12.5F, System.Drawing.FontStyle.Italic);
+            this.lblTagline.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(175)))), ((int)(((byte)(200)))));
+            this.lblTagline.Location = new System.Drawing.Point(1, 164);
+            this.lblTagline.Name = "lblTagline";
+            this.lblTagline.Size = new System.Drawing.Size(358, 46);
+            this.lblTagline.TabIndex = 0;
+            this.lblTagline.Text = "#vui là chính, thắng là mười";
+            this.lblTagline.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // panelRight
+            // 
+            this.panelRight.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(24)))), ((int)(((byte)(36)))));
+            this.panelRight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelRight.Controls.Add(this.lblTitle);
+            this.panelRight.Controls.Add(this.txtUsername);
+            this.panelRight.Controls.Add(this.txtPassword);
+            this.panelRight.Controls.Add(this.btnTogglePwd);
+            this.panelRight.Controls.Add(this.chkRemember);
+            this.panelRight.Controls.Add(this.btnLogin);
+            this.panelRight.Controls.Add(this.btnRegister);
+            this.panelRight.Controls.Add(this.lblError);
+            this.panelRight.Location = new System.Drawing.Point(481, 48);
+            this.panelRight.Name = "panelRight";
+            this.panelRight.Size = new System.Drawing.Size(520, 560);
+            this.panelRight.TabIndex = 1;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI Semibold", 22F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(36, 32);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(430, 52);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "ĐĂNG NHẬP";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(46)))));
+            this.txtUsername.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtUsername.Font = new System.Drawing.Font("Segoe UI", 12.5F);
+            this.txtUsername.ForeColor = System.Drawing.Color.Gray;
+            this.txtUsername.Location = new System.Drawing.Point(40, 120);
+            this.txtUsername.Margin = new System.Windows.Forms.Padding(0);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(440, 41);
+            this.txtUsername.TabIndex = 1;
+            this.txtUsername.Tag = "Tên đăng nhập hoặc email";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(46)))));
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPassword.Font = new System.Drawing.Font("Segoe UI", 12.5F);
+            this.txtPassword.ForeColor = System.Drawing.Color.Gray;
+            this.txtPassword.Location = new System.Drawing.Point(40, 180);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(0);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(396, 41);
+            this.txtPassword.TabIndex = 2;
+            this.txtPassword.Tag = "Mật khẩu";
+            // 
+            // btnTogglePwd
+            // 
+            this.btnTogglePwd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(46)))));
+            this.btnTogglePwd.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnTogglePwd.FlatAppearance.BorderSize = 0;
+            this.btnTogglePwd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTogglePwd.Font = new System.Drawing.Font("Segoe UI Emoji", 12F);
+            this.btnTogglePwd.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(210)))), ((int)(((byte)(230)))));
+            this.btnTogglePwd.Location = new System.Drawing.Point(440, 178);
+            this.btnTogglePwd.Name = "btnTogglePwd";
+            this.btnTogglePwd.Size = new System.Drawing.Size(40, 44);
+            this.btnTogglePwd.TabIndex = 3;
+            this.btnTogglePwd.Text = "👁";
+            this.btnTogglePwd.UseVisualStyleBackColor = false;
+            // 
+            // chkRemember
+            // 
+            this.chkRemember.AutoSize = true;
+            this.chkRemember.Font = new System.Drawing.Font("Segoe UI", 10.5F);
+            this.chkRemember.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(195)))), ((int)(((byte)(215)))));
+            this.chkRemember.Location = new System.Drawing.Point(40, 238);
+            this.chkRemember.Name = "chkRemember";
+            this.chkRemember.Size = new System.Drawing.Size(143, 34);
+            this.chkRemember.TabIndex = 4;
+            this.chkRemember.Text = "Ghi nhớ tôi";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(132)))), ((int)(((byte)(255)))));
+            this.btnLogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLogin.FlatAppearance.BorderSize = 0;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Segoe UI Semibold", 12.5F, System.Drawing.FontStyle.Bold);
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(40, 286);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(440, 48);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "Đăng nhập";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.BackColor = System.Drawing.Color.Transparent;
+            this.btnRegister.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRegister.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(95)))), ((int)(((byte)(120)))));
+            this.btnRegister.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegister.Font = new System.Drawing.Font("Segoe UI", 11.5F, System.Drawing.FontStyle.Bold);
+            this.btnRegister.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(220)))), ((int)(((byte)(235)))));
+            this.btnRegister.Location = new System.Drawing.Point(40, 344);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(440, 46);
+            this.btnRegister.TabIndex = 6;
+            this.btnRegister.Text = "Tạo tài khoản mới";
+            this.btnRegister.UseVisualStyleBackColor = false;
+            // 
+            // lblError
+            // 
+            this.lblError.Font = new System.Drawing.Font("Segoe UI", 10.5F);
+            this.lblError.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(115)))), ((int)(((byte)(115)))));
+            this.lblError.Location = new System.Drawing.Point(40, 404);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(440, 46);
+            this.lblError.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Client.Properties.Resources.Gemini_Generated_Image_c3l9vzc3l9vzc3l9_removebg_preview;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(362, 162);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(18)))), ((int)(((byte)(26)))));
+            this.ClientSize = new System.Drawing.Size(1080, 680);
+            this.Controls.Add(this.panelShell);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LoginForm";
+            this.Padding = new System.Windows.Forms.Padding(24);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "HappyCaro - Đăng nhập";
+            this.panelShell.ResumeLayout(false);
+            this.panelLeft.ResumeLayout(false);
+            this.panelRight.ResumeLayout(false);
+            this.panelRight.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
 
+        }
         #endregion
+
+        private PictureBox pictureBox1;
     }
 }
--- BaiTapTuan04_MultipleSocket/UserManageApp/Client/Forms/LoginForm.cs
+++ BaiTapTuan04_MultipleSocket/UserManageApp/Client/Forms/LoginForm.cs
@@ -1,20 +1,172 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿using Client; // TcpClientHelper + Security
+using Exercise3_Client.Utils;
+using System;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing.Drawing2D;
+using System.Runtime.InteropServices;
+using System.Text.Json;
 using System.Windows.Forms;
+using SecurityUtil = Exercise3_Client.Utils.Security;
 
 namespace Client.Forms
 {
     public partial class LoginForm : Form
     {
+        private const string SERVER_HOST = "127.0.0.1";
+        private const int SERVER_PORT = 9000;
+
+        private TcpClientHelper _tcpClient;
+
         public LoginForm()
         {
             InitializeComponent();
+
+            _tcpClient = new TcpClientHelper();
+
+            // nền gradient nhẹ (OK với Designer)
+            this.Paint += LoginForm_Paint;
+
+            // kéo form không viền
+            this.MouseDown += (s, e) =>
+            {
+                if (e.Button == MouseButtons.Left) NativeDrag(this.Handle);
+            };
+
+            // placeholder thủ công cho .NET Framework
+            SetupPlaceholder(txtUsername, (string)txtUsername.Tag, false);
+            SetupPlaceholder(txtPassword, (string)txtPassword.Tag, true);
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            this.AcceptButton = btnLogin;
+        }
+
+        private void LoginForm_Paint(object sender, PaintEventArgs e)
+        {
+            using (var br = new LinearGradientBrush(
+                this.ClientRectangle,
+                Color.FromArgb(18, 24, 36),
+                Color.FromArgb(10, 14, 22),
+                90f))
+            {
+                e.Graphics.FillRectangle(br, this.ClientRectangle);
+            }
+        }
+
+        private async void btnLogin_Click(object sender, EventArgs e)
+        {
+            lblError.Text = string.Empty;
+
+            var username = GetValue(txtUsername);
+            var password = GetValue(txtPassword);
+
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+            {
+                lblError.Text = "Vui lòng nhập tên đăng nhập và mật khẩu.";
+                return;
+            }
+
+            var reqObj = new
+            {
+                Action = "LOGIN",
+                Data = new { Username = username, Password = Security.Sha256Hex(password) }
+            };
+
+            try
+            {
+                await _tcpClient.ConnectAsync(SERVER_HOST, SERVER_PORT);
+
+                string reqJson = JsonSerializer.Serialize(reqObj);
+                string respJson = await _tcpClient.SendAsync(reqJson);
+
+                var resp = JsonDocument.Parse(respJson).RootElement;
+                bool success = resp.GetProperty("Success").GetBoolean();
+                string message = resp.GetProperty("Message").GetString();
+
+                if (!success)
+                {
+                    lblError.Text = "Đăng nhập thất bại: " + message;
+                    return;
+                }
+
+                var data = resp.GetProperty("Data");
+                string token = data.GetProperty("Token").GetString();
+                string userJson = data.GetProperty("User").GetRawText();
+
+                var main = new MainForm(token, userJson, _tcpClient);
+                this.Hide();
+
+                main.FormClosed += (s2, e2) =>
+                {
+                    try { _tcpClient?.Dispose(); } catch { }
+                    _tcpClient = new TcpClientHelper(); // để đăng nhập lại nếu cần
+                    this.Show();
+                };
+
+                main.Show();
+            }
+            catch (Exception ex)
+            {
+                lblError.Text = "Lỗi kết nối: " + ex.Message;
+            }
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            new RegisterForm().ShowDialog();
+        }
+
+        private void btnTogglePwd_Click(object sender, EventArgs e)
+        {
+            bool isPlaceholder = txtPassword.ForeColor == Color.Gray &&
+                                 txtPassword.Text == (string)txtPassword.Tag;
+            if (isPlaceholder) return;
+
+            txtPassword.UseSystemPasswordChar = !txtPassword.UseSystemPasswordChar;
+            btnTogglePwd.Text = txtPassword.UseSystemPasswordChar ? "👁" : "🚫";
+            txtPassword.Focus();
+        }
+
+        // ---- Placeholder helpers ----
+        private static void SetupPlaceholder(TextBox tb, string placeholder, bool isPassword)
+        {
+            void SetPh()
+            {
+                tb.ForeColor = Color.Gray;
+                tb.Text = placeholder;
+                if (isPassword) tb.UseSystemPasswordChar = false;
+            }
+            void UnsetPh()
+            {
+                tb.ForeColor = Color.White;
+                tb.Text = string.Empty;
+                if (isPassword) tb.UseSystemPasswordChar = true;
+            }
+
+            SetPh();
+            tb.GotFocus += (s, e) =>
+            {
+                if (tb.ForeColor == Color.Gray && tb.Text == placeholder) UnsetPh();
+            };
+            tb.LostFocus += (s, e) =>
+            {
+                if (string.IsNullOrWhiteSpace(tb.Text)) SetPh();
+            };
+        }
+
+        private static string GetValue(TextBox tb)
+            => tb.ForeColor == Color.Gray ? string.Empty : tb.Text.Trim();
+
+        // ---- Kéo form không viền ----
+        [DllImport("user32.dll")] private static extern bool ReleaseCapture();
+        [DllImport("user32.dll")] private static extern IntPtr SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        private static void NativeDrag(IntPtr handle)
+        {
+            const int WM_NCLBUTTONDOWN = 0xA1;
+            const int HTCAPTION = 0x2;
+            ReleaseCapture();
+            SendMessage(handle, WM_NCLBUTTONDOWN, HTCAPTION, 0);
         }
     }
 }
